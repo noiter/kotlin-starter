bba7b9a36e56c8c18ab153dcbe637cc21b2de221
noiter <zm_uestc@hotmail.com>
2017-12-25 17:10:32 +0800

    [Ming] Init a git repo

---
+++ src/main/kotlin/starter/kotlin/App.kt
@@ -0,0 +1,18 @@
+package starter.kotlin
+
+import org.jooby.Jooby.*
+import org.jooby.Kooby
+
+class App : Kooby({
+    get {
+        val name = param("name").value("Kotlin")
+        "Hello $name!"
+    }
+})
+
+/**
+ * Run application:
+ */
+fun main(args: Array<String>) {
+    run(::App, args)
+}
